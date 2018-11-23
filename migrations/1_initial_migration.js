c4bfab603428d9d96bc22c3e01b5c5d842d2c651
Patricio Lopez <patriciolopez@MacBook-Pro-de-Patricio.local>
2018-11-23 01:35:05 -0300

    deploy inicial

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,9 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var House = artifacts.require("./House.sol");
+var KongrioToken = artifacts.require("./KongrioToken.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(House);
+  deployer.deploy(KongrioToken);
 };
